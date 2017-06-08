b7d41bd2ac0b2e1a0f70cf0519ece78c4e55014b
Gabriel Casarin <gabriel.casarin@hotmail.com>
2017-06-08 08:10:05 -0300

    add method to_vect     modified: models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from py2neo.ogm import GraphObject, Property, RelatedTo
+import numpy as np
 
 
 class Match(GraphObject):
@@ -16,3 +17,21 @@ class State(GraphObject):
     qtd_visitas = Property()
 
     moved_to = RelatedTo("State")
+
+    def to_vect(self):
+        tam = 33 + len(self.tabuleiro)**2
+        vect = np.ndarray((1, tam))
+        vect[0][0] = self.turno
+        k = 1
+        for i in range(len(self.samurais)):
+            for j in range(5):
+                vect[0][k] = self.samurais[i][j]
+                k += 1
+        for i in range(len(self.tabuleiro)):
+            for j in range(len(self.tabuleiro)):
+                vect[0][k] = self.tabuleiro[i][j]
+                k += 1
+        vect[0][k] = self.budget
+        k += 1
+        vect[0][k] = self.player
+        return vect
